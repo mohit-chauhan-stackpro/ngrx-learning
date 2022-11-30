afccbdceafa534626a3f962a47372ffe7c8310b8
mohit-chauhan-stackpro <mohit.chauhan@stackpro.in>
2022-11-30 11:27:38 +0530

    added comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/layout/dashboard/dashboard.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { YoutubeLayoutComponent } from './components/layout/youtube-layout/youtube-layout.component';
+import { PostComponent } from './containers/post/post.component';
+import { UsersComponent } from './containers/users/users.component';
+import { ViewUserComponent } from './containers/view-user/view-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    YoutubeLayoutComponent,
+    PostComponent,
+    UsersComponent,
+    ViewUserComponent
   ],
   imports: [
     BrowserModule,
